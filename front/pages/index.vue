79cddf17f9f19fd1175f3f176b6475e04d3fc623
佐藤文彦 <fsato@ciaoinc.jp>
2020-01-10 17:58:33 +0900

    初回プッシュ

---
+++ front/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div> {{data}} </div>
+</template>
+<script>
+  export default{
+    async asyncData({app}){
+      const data = await app.$axios.$get('http://localhost/api')
+      return {data};
+    }
+  }
+</script>
